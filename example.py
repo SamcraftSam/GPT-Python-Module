622a732a192d1bfe75e3fe95392f72b1a0df88f8
SamcraftSam <71342098+SamcraftSam@users.noreply.github.com>
2023-05-24 01:21:53 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,12 +1,9 @@
-from gpt3 import *
+from gptgenerator import *
 
 prompt_setup = "У кожному наступному повідомленні я надаватиму тобі певну новину у вигляді статті. Ти маєш творчо підійти до завдання, і перефразувати цю новину, зберігши зміст. Просто перефразуй її. Відповідай тільки результатом, без передмов. Новина має відповідати на питання 'Що? Де? Як? Коли? Чому?' Розмір статті до 1000 символів"
 with open("test.txt", "r") as f:
     prompt_new = f.read() #test purposes
 
-messags = [
-    {"role": "assistant", "content": prompt_setup},
-]
 print("Its test script for gpt3 generator library!")
 print(header(prompt_new))
 print("#########################################################################################")
